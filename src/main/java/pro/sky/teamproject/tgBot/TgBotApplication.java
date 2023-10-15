0e568cf18083b2af18a204f32821bf4dbd03744e
beshik7 <tommypdx7@gmail.com>
2023-10-14 20:36:46 -0700

    Master commit

---
+++ src/main/java/pro/sky/teamproject/tgBot/TgBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.teamproject.tgBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/teamproject/tgBot/TgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.teamproject.tgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
