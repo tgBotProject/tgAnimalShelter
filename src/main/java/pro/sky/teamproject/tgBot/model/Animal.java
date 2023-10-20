a129e0722a780e95fcdf4ae04b30176b5bba9af3
Ldv236 <Ldv236@yandex.ru>
2023-10-20 21:30:06 +0200

    10: entity user, adoption, dependency, properties

---
+++ src/main/java/pro/sky/teamproject/tgBot/model/Animal.java
@@ -0,0 +1,19 @@
+package pro.sky.teamproject.tgBot.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@Entity
+@Table(name = "animals")
+public class Animal {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
